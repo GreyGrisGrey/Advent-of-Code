75b9fa5fa9531fd99ee033c2ce356c592b53f560
Grey <myusernamesuckssorry@gmail.com>
2025-05-31 18:10:59 -0230

    2015 day 23 update

--- 2015/day23.py
+++ 2015/day23.py
@@ -1,38 +1,28 @@
-def part1():
-    instructs = []
-    for i in open("in.txt"):
-        instructs.append(i.strip("\n").split(" "))
+def checkRes(vals, instructs):
     curr = 0
-    vals = [1, 0]
-    steps = 0
     while curr >= 0 and curr < len(instructs):
-        steps += 1
         if instructs[curr][1][0] in "ab":
             valIndex = 0 if instructs[curr][1][0] == "a" else 1
         match instructs[curr][0]:
             case "hlf":
-                vals[valIndex] /= 2
-                vals[valIndex] = int(vals[valIndex])
-                curr += 1
+                vals[valIndex] = int(vals[valIndex]/2)
             case "tpl":
                 vals[valIndex] *= 3
-                curr += 1
             case "inc":
                 vals[valIndex] += 1
-                curr += 1
             case "jmp":
-                curr += int(instructs[curr][1])
+                curr += int(instructs[curr][1]) - 1
             case "jie":
                 if vals[valIndex] % 2 == 0:
-                    curr += int(instructs[curr][2])
-                else:
-                    curr += 1
+                    curr += int(instructs[curr][2]) - 1
             case "jio":
                 if vals[valIndex] == 1:
-                    curr += int(instructs[curr][2])
-                else:
-                    curr += 1
-        print(curr, vals)
-    return vals
+                    curr += int(instructs[curr][2]) - 1
+        curr += 1
+    return vals[1]
 
-print(part1())
+def partBoth():
+    instructs = list(map(lambda x: x.split(" "), open("in.txt").read().split("\n")))
+    return checkRes([0, 0], instructs), checkRes([1, 0], instructs)
+
+print(partBoth())
