f938e8744c797f6973b13208e3adbead95a4eda5
Grey <myusernamesuckssorry@gmail.com>
2025-06-16 18:59:51 -0230

    2022 day 10

---
+++ 2022/day10.ts
@@ -0,0 +1,49 @@
+import * as fs from "fs";
+
+
+function part1(): number {
+    let count = 0, a = fs.readFileSync("in.txt", "utf8").split("\r\n"), X = 1
+    let cycle = 0, cycleCount = 1
+    for (let i = 0; i < a.length; i++) {
+        if (cycle >= 18) {
+            count += 20 * X * cycleCount
+            console.log(count, cycle, cycleCount, X, 20 * X * cycleCount)
+            cycle -= 40
+            cycleCount += 2
+        }
+        if (a[i] != "noop") {
+            X += parseInt(a[i].split(" ")[1])
+            cycle += 2
+        } else {
+            cycle += 1
+        }
+        console.log(cycle, X)
+    }
+    return count
+}
+
+function part2(): void {
+    let count = 0, a = fs.readFileSync("in.txt", "utf8").split("\r\n"), X = 1, cycle = -1, currInst = -1, currString = ""
+    while (currInst < a.length - 1) {
+        if (count == 0) {
+            currInst += 1
+            count = a[currInst] == "noop" ? 1 : 2
+        }
+        while (count > 0) {
+            count -= 1
+            cycle += 1
+            currString = currString.concat((Math.abs(X - cycle) < 2) ? "#" : ".")
+            if (cycle == 39) {
+                console.log(currString)
+                currString = ""
+                cycle = -1
+            }
+            if (count == 0 && a[currInst] != "noop") {
+                X += parseInt(a[currInst].split(" ")[1])
+            }
+        }
+    }
+}
+
+console.log(part1())
+part2()
