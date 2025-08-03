48aa6d4d96aa46986ee78e33987dc0e5d4d3779b
Grey <myusernamesuckssorry@gmail.com>
2025-08-03 15:31:51 -0230

    2019 day 5

---
+++ 2019/day5.py
@@ -0,0 +1,7 @@
+from intCode import intMain
+
+if __name__ == "__main__":
+    print("Part 1:\n------")
+    intMain(1, 0, 5)
+    print("Part 2:\n------")
+    intMain(5, 0, 5)
--- 2019/intCode.py
+++ 2019/intCode.py
@@ -1,3 +1,5 @@
+from math import floor
+
 def initializeMapping():
     spaces = {}
     curr = 0
@@ -6,28 +8,68 @@ def initializeMapping():
         curr += 1
     return spaces
 
-def runOp(index, spaces):
-    match spaces[index]:
-        case 99:
-            return 0
-        case 1:
-            spaces[spaces[index + 3]] = spaces[spaces[index + 1]] + spaces[spaces[index + 2]]
-        case 2: 
-            spaces[spaces[index + 3]] = spaces[spaces[index + 1]] * spaces[spaces[index + 2]]
-        case _:
-            return 2
-    return 1
+def getDigit(num, index):
+    return floor(num/10**index) % 10
 
-def intMain(input1, input2):
+def getInstruct(num):
+    return num % 100
+
+def getWords(index, spaces):
+    noun = spaces[spaces[index + 1]] if getDigit(spaces[index], 2) == 0 else spaces[index + 1]
+    verb = spaces[spaces[index + 2]] if getDigit(spaces[index], 3) == 0 else spaces[index + 2]
+    return noun, verb
+
+def runOp(index, input, spaces):
+    instruct = getInstruct(spaces[index])
+    match instruct:
+        case 99:
+            return (0, 0)
+        case 1:
+            noun, verb = getWords(index, spaces)
+            spaces[spaces[index + 3]] = noun + verb
+            next = index + 4
+        case 2: 
+            noun, verb = getWords(index, spaces)
+            spaces[spaces[index + 3]] = noun * verb
+            next = index + 4
+        case 3:
+            spaces[spaces[index + 1]] = input
+            next = index + 2
+        case 4:
+            print(spaces[spaces[index + 1]])
+            next = index + 2
+        case 5:
+            noun, verb = getWords(index, spaces)
+            next = verb if noun != 0 else index + 3
+        case 6:
+            noun, verb = getWords(index, spaces)
+            next = verb if noun == 0 else index + 3
+        case 7:
+            noun, verb = getWords(index, spaces)
+            spaces[spaces[index + 3]] = 1 if noun < verb else 0
+            next = index + 4
+        case 8:
+            noun, verb = getWords(index, spaces)
+            spaces[spaces[index + 3]] = 1 if noun == verb else 0
+            next = index + 4
+        case _:
+            print("Error: Unknown Opcode")
+            print("Opcode:", spaces[index], "Location:", index)
+            return (2, 0)
+    return (1, next)
+
+def intMain(input1, input2, day):
     spaces = initializeMapping()
     index = 0
     res = 1
-    spaces[1] = input1
-    spaces[2] = input2
+    if day == 2:
+        spaces[1] = input1
+        spaces[2] = input2
     while res == 1:
-        res = runOp(index, spaces)
-        index += 4
+        if day == 5:
+            res, index = runOp(index, input1, spaces)
+        else:
+            res, index = runOp(index, 0, spaces)
     if res == 2:
-        print("Error: Unknown Opcode")
         return {}
     return spaces
