1d4edf15d0b43789862a66f4f810b54f8ef5c1e8
Grey <myusernamesuckssorry@gmail.com>
2025-08-09 11:00:05 -0230

    2019 day 16

---
+++ 2019/day16.py
@@ -0,0 +1,62 @@
+def calcDigit(inputString, repeater, i):
+        currIndex = 0
+        remaining = i
+        res = 0
+        count = 0
+        while count < len(inputString):
+            if currIndex == 0:
+                count += i
+                remaining = i + 1
+                currIndex = 1
+                if count >= len(inputString):
+                    break
+            else:
+                res += int(inputString[count]) * repeater[currIndex]
+                remaining -= 1
+                count += 1
+                if remaining <= 0:
+                    count += i + 1
+                    remaining = i + 1
+                    currIndex = (currIndex + 2) % 4
+        return abs(res) % 10
+
+def runPhase(inputString, repeater, outList):
+    for i in range(len(inputString)):
+        outList.append(str(calcDigit(inputString, repeater, i)))
+    return "".join(outList)
+            
+
+def part1():
+    repeater = [0, 1, 0, -1]
+    inputString = open("in.txt").read()
+    outList = []
+    for i in range(100):
+        inputString = runPhase(inputString, repeater, outList)
+        outList = []
+    print("Part 1:", inputString[0:8:])
+
+# works if offset > input length/2 and in no other context.
+# also quite slow.
+def part2():
+    inputString = open("in.txt").read()
+    offset = int(inputString[0:7:])
+    inputString *= 10000
+    for i in range(100):
+        nextLevel = []
+        if i == 0:
+            total = 0
+            for j in range(len(inputString) - offset):
+                total += int(inputString[len(inputString) - 1 - j])
+                nextLevel.append(str(abs(total) % 10))
+        else:
+            total = 0
+            for j in range(len(inputString)):
+                total += int(inputString[len(inputString) - 1 - j])
+                nextLevel.append(str(abs(total) % 10))
+        nextLevel.reverse()
+        inputString = nextLevel
+    print("Part 2:", "".join(inputString[0:8:]))
+
+if __name__ == "__main__":
+    part1()
+    part2()
