5a3fa06e2ccc6609774e91703759ae050bb5b5f7
Grey <myusernamesuckssorry@gmail.com>
2025-08-12 18:59:03 -0230

    2019 day 19

---
+++ 2019/day19.py
@@ -0,0 +1,60 @@
+from intCode import intMachine
+
+def part1():
+    count = 0
+    spaces = {}
+    for i in range(50):
+        for j in range(50):
+            machine = intMachine(19)
+            machine.addInputs([i, j])
+            res = machine.run(getOuts = True)
+            if res[1] != 0:
+                count += 1
+    return count
+
+# takes a minute
+def part2():
+    currX = 0
+    currY = 870
+    while True:
+        machine = intMachine(19)
+        machine.addInputs([currX, currY])
+        res = machine.run(getOuts = True)
+        if res[1] == 1:
+            break
+        else:
+            currX += 1
+    while True:
+        while True:
+            machine = intMachine(19)
+            machine.addInputs([currX, currY])
+            res = machine.run(getOuts = True)
+            if res[1] == 1:
+                break
+            else:
+                currX += 1
+        size = 0
+        while True:
+            machine = intMachine(19)
+            machine.addInputs([currX + size, currY])
+            res = machine.run(getOuts = True)
+            if res[1] == 0:
+                break
+            size += 1
+        # check where the guy 100 down is, duh
+        baseX = currX
+        while True:
+            machine = intMachine(19)
+            machine.addInputs([baseX, currY + 99])
+            res = machine.run(getOuts = True)
+            if res[1] == 1:
+                break
+            else:
+                baseX += 1
+        if size - (baseX - currX) >= 100:
+            return (baseX * 10000) + currY
+        currY += 1
+
+if __name__ == "__main__":
+    print("Part 1:", part1())
+    print("Part 2:", part2())
